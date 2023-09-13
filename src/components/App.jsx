260c96a78702ece4efdb41cb59fac772d49269e0
Shah Alam <shahalambdh3@gmail.com>
2023-09-13 08:13:02 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function App() {
+
+  const [fName , setFName] = useState("");
+  const [lName, setLName] =useState("");
+
+
+  function updateFName(event){
+    setFName(event.target.value);
+  
+  }
+
+  function updateLName(event){
+    setLName(event.target.value);
+  }
+
+
+
+  return (
+    <div className="container">
+      <h1>Hello {fName} {lName} </h1>
+      <form>
+        <input 
+        onChange={updateFName}
+        name="fName" 
+        placeholder="First Name" 
+        value={fName}
+        />
+        <input 
+        onChange={updateLName}
+        name="lName" 
+        placeholder="Last Name"
+        value={lName}
+         />
+        <button type ="submit" >Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
